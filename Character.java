e65a02e1749ab595c98e1d91db38972b274cb06a
Elen Galstyan <elengalstyan@Elens-MacBook-Air.local>
2024-03-26 18:18:51 +0400

    Added current Character methods

--- Character.java
+++ Character.java
@@ -1,9 +1,14 @@
-public class Character{
-    /**
-     * This class is the default character. It is the parent of the main 5 characters. Every Character
-     * has 3 main methods(+ some other helping methods): influence, action and counterAction.
-     * influence: returns the character type.
-     * action: a method to take an action with a corresponding effect.
-     * counterAction: a method to block the action.
-     */
+/**
+ * This class is the default character. It is the parent of the main 5 characters. Every Character
+ * has 3 main methods(+ some other helping methods): influence, action and counterAction.
+ * influence: returns the character type.
+ * action: a method to take an action with a corresponding effect.
+ * counterAction: a method to block the action.
+ */
+public abstact class Character{
+    public abstract action();
+    public abstract counterAction();
+    public getCharacter(){
+        return this.getClass().getSimpleName();
+    }
 }
