bc381f165284bd04bd4f4c160d5ed20139aecf26
Lussssya <163138649+Lussssya@users.noreply.github.com>
2024-03-26 18:27:21 +0400

    Update Ambassador.java

--- Ambassador.java
+++ Ambassador.java
@@ -1,7 +1,16 @@
+/**
+ * A child class of Character.
+ * action: to exchange, which means to draw 2 characters and put 2 back.
+ * counterAction: to block stealing.
+ */
 public class Ambassador extends Character{
-    /**
-     * A child class of Character.
-     * action: to exchange, which means to draw 2 characters and put 2 back.
-     * counterAction: to block stealing.
-     */
+    @Override
+    public void action() {
+        
+    }
+
+    @Override
+    public void counterAction() {
+
+    }
 }
