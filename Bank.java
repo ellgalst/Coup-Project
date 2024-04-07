bb08de83c75978d37e47c67dd35a0dee9a38652b
Elen Galstyan <elengalstyan@Elens-MacBook-Air.local>
2024-04-07 20:31:35 +0400

    Added some methods and made minor cahnges

--- Bank.java
+++ Bank.java
@@ -3,4 +3,29 @@
  */
 public class Bank{
 
+    public void foreignAid(Player player){
+        collectTwoCoins(player);
+        isBlocked(player.getCharacter); // some method necessary to get the character for this method.
+    }
+
+
+    //might be private
+    public void collectTwoCoins(Player player){
+
+    }
+
+    //might be private
+    public boolean isBlocked(Character current){
+        //  method implementation missing:
+
+        return true;
+    }
+
+    public void  income(Player player){
+        getOneCoin(player);
+    }
+
+    public void getOneCoin(Player player){
+        
+    }
 }
