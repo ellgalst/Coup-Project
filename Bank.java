4eef86845f577a2e090c09285f00ff09dbe6d5a4
lusyasargsyan <lusya_sargsyan@edu.aua.am>
2024-04-13 15:58:58 +0400

    testing

--- Bank.java
+++ Bank.java
@@ -2,7 +2,7 @@
  * A class to manage financial transactions.
  */
 public class Bank{
-
+    // test
     public void foreignAid(Player player){
         collectTwoCoins(player);
         isBlocked(player.getCharacter); // some method necessary to get the character for this method.
