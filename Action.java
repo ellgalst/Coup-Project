a464d8b23f1a32d16c44da3db5547934f70146ce
lusyasargsyan <lusya_sargsyan@edu.aua.am>
2024-04-13 21:23:19 +0400

    correct game logic

---
+++ Action.java
@@ -0,0 +1,34 @@
+
+public class Action {
+
+    // enum Types for checks
+    public enum Types {
+        STEAL,
+        EXCHANGE,
+        ASSASSINATE,
+        TAX,
+        COUP,
+        INCOME,
+        FOREIGNAID
+    }
+
+    // all the actions logic
+    public void TAX() {
+    }
+
+    public void EXCHANGE() {}
+
+    public void STEAL(){
+
+    }
+    public void ASSASSINATE(){}
+
+    public void Income(){}
+
+    public  void ForeignAid(){}
+
+    public void Coup(){}
+
+
+}
+
