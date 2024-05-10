9bd437786fbe8a437333e602942bbb599ee87216
lusyasargsyan <lusya_sargsyan@edu.aua.am>
2024-05-10 21:37:32 +0400

    main

--- src/Main.java
+++ src/Main.java
@@ -18,89 +18,89 @@ public class Main {
      * The entry point of the Coup game.
      */
     public static void main(final String[] args) {
-        if(args.length == 0) {
+        if (args.length == 0) {
             SwingUtilities.invokeLater(new Runnable() {
                 @Override
                 public void run() {
                     SwingUtilities.invokeLater(CoupGUI::new);
                 }
             });
-        }
-        else{
-                try {
-                    Scanner scanner = new Scanner(System.in);
-                    System.out.println("Enter a number from 2 to 6, which will be the number of players you want to play today!");
-                    int numberOfPlayers = scanner.nextInt();
-                    if (numberOfPlayers >= 7)
-                        throw new InvalidNumberOfPlayersException("invalid Number of players, try again");
+        } else {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter a number from 2 to 6, which will be the number of players you want to play today!");
+                int numberOfPlayers = scanner.nextInt();
+                if (numberOfPlayers >= 7)
+                    throw new InvalidNumberOfPlayersException("invalid Number of players, try again");
 
-                    Game myGame = new Game(numberOfPlayers);
-                    myGame.start();
+                Game myGame = new Game(numberOfPlayers);
+                myGame.start();
 
-            while (myGame.isNotOver()) {
-                for (BasePerformer player : myGame.players) {
-                    myGame.players = new ArrayList<>(myGame.updatePlayers(player));
-                    if (myGame.players.size() == 1) {
-                        break;
-                    }
-                    System.out.println(myGame.players);
-                    System.out.println("Player " + player + "'s turn!");
-                    Action.Types playersChoice = player.act(myGame);
-                    BasePerformer target = null;
-                    if (playersChoice == Action.Types.STEAL || playersChoice == Action.Types.ASSASSINATE || playersChoice == Action.Types.COUP) {
-                        if (player instanceof Player) {
-                            System.out.println("Please, choose the player you want to perform the action on: ");
-                            target = Player.getTarget(myGame.players);
-                        } else {
-                            ArrayList<BasePerformer> currentPlayers = new ArrayList<>(myGame.players);
-                            int randomIndex = (int) (Math.random() * (currentPlayers.size()));
-                            target = currentPlayers.get(randomIndex);
+                while (myGame.isNotOver()) {
+                    for (BasePerformer player : myGame.players) {
+                        myGame.players = new ArrayList<>(myGame.updatePlayers(player));
+                        if (myGame.players.size() == 1) {
+                            break;
                         }
-                    }
-                    BasePerformer challenger = null;
-                    if (!(playersChoice == Action.Types.FOREIGN_AID || playersChoice == Action.Types.INCOME)) {
-                         challenger = Game.chooseChallenger(myGame.players, player);
-                    }
-                    if (challenger != null) {
-                        if (!challenger.challenges(player, deck, playersChoice, true)) {
-                            if (target != null) {
-                                if (!target.block(player, deck, playersChoice)) {
+                        System.out.println(myGame.players);
+                        System.out.println("Player " + player + "'s turn!");
+                        Action.Types playersChoice = player.act(myGame);
+                        BasePerformer target = null;
+                        if (playersChoice == Action.Types.STEAL || playersChoice == Action.Types.ASSASSINATE || playersChoice == Action.Types.COUP) {
+                            if (player instanceof Player) {
+                                System.out.println("Please, choose the player you want to perform the action on: ");
+                                target = Player.getTarget(myGame.players);
+                            } else {
+                                ArrayList<BasePerformer> currentPlayers = new ArrayList<>(myGame.players);
+                                int randomIndex = (int) (Math.random() * (currentPlayers.size()));
+                                target = currentPlayers.get(randomIndex);
+                            }
+                        }
+                        BasePerformer challenger = null;
+                        if (!(playersChoice == Action.Types.FOREIGN_AID || playersChoice == Action.Types.INCOME)) {
+                            challenger = Game.chooseChallenger(myGame.players, player);
+                        }
+                        if (challenger != null) {
+                            if (!challenger.challenges(player, deck, playersChoice, true)) {
+                                if (target != null) {
+                                    if (!target.block(player, deck, playersChoice)) {
+                                        actionWorks(player, playersChoice, target);
+                                        myGame.players = new ArrayList<>(myGame.updatePlayers(player));
+                                        if (myGame.players.size() == 1) {
+                                            break;
+                                        }
+                                    }
+                                } else {
                                     actionWorks(player, playersChoice, target);
+
                                     myGame.players = new ArrayList<>(myGame.updatePlayers(player));
                                     if (myGame.players.size() == 1) {
                                         break;
                                     }
                                 }
                             } else {
-                                actionWorks(player, playersChoice, target);
-
-                                myGame.players = new ArrayList<>(myGame.updatePlayers(player));
-                                if (myGame.players.size() == 1) {
-                                    break;
-                                }
+                                System.out.println("Challenger succeeded! " + player.getName() + "'s action is not performed.");
                             }
                         } else {
-                            System.out.println("Challenger succeeded! " + player.getName() + "'s action is not performed.");
+                            actionWorks(player, playersChoice, target);
+                            myGame.players = new ArrayList<>(myGame.updatePlayers(player));
                         }
-                    } else {
-                        actionWorks(player, playersChoice, target);
+
+
                         myGame.players = new ArrayList<>(myGame.updatePlayers(player));
+                        if (myGame.players.size() == 1) {
+                            break;
+                        }
+
+                        myGame.printData();
+
                     }
-
-
-                    myGame.players = new ArrayList<>(myGame.updatePlayers(player));
-                    if (myGame.players.size() == 1) {
-                        break;
-                    }
-
-                    myGame.printData();
-
                 }
+            } catch (InvalidNumberOfPlayersException e) {
+                System.out.println("The number of players is invalid, try again");
             }
-        } catch (InvalidNumberOfPlayersException e) {
-            System.out.println("The number of players is invalid, try again");
+            System.out.println("The game is over!");
         }
-        System.out.println("The game is over!");
     }
     private static void actionWorks (BasePerformer player, Action.Types playersChoice, BasePerformer target) {
         boolean works = Action.performAction(player, playersChoice, target);
